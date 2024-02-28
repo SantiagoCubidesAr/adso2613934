eb54a925b83569a6b3685ef4603d0283ad58af07
SantiagoCubidesAr <santiagocubidesar@gmail.com>
2024-02-27 20:30:45 -0500

    El ejemplo visibility esta listo

--- 01-oop/03-visibility.php
+++ 01-oop/03-visibility.php
@@ -18,7 +18,7 @@
 
             h2 {
                 margin: 0;
-            } 
+            }
 
             form {
                 border: 2px solid #fff6;
@@ -51,74 +51,106 @@
                 }
 
             }
+
+            table {
+                border-collapse: collapse;
+
+                tr {
+                    td {
+                        background-color: #fff1;
+                        padding: 8px;
+                        border: 1px solid #fff;
+                    } 
+                }
+
+                tr:nth-child(even) td:nth-child(even){
+                    background-color: #0004ff;
+                }
+                tr:nth-child(odd) td:nth-child(odd){
+                    background-color: #0004ff;
+                }
+            }
         }
     </style>
 </head>
 
 <body>
-<nav class="controls">
+    <nav class="controls">
         <a href="index.html">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM231 127c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-71 71L376 232c13.3 0 24 10.7 24 24s-10.7 24-24 24l-182.1 0 71 71c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L119 273c-9.4-9.4-9.4-24.6 0-33.9L231 127z"/></svg>    
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                <path fill="#ffffff" d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM231 127c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-71 71L376 232c13.3 0 24 10.7 24 24s-10.7 24-24 24l-182.1 0 71 71c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L119 273c-9.4-9.4-9.4-24.6 0-33.9L231 127z" />
+            </svg>
         </a>
     </nav>
     <main>
         <h1>03- Visibility</h1>
         <section>
-            <?php
-                class TableMaker {
-                    // Attributes
-                    private $nr; //Number of Rows
-                    private $nc; //Number od Columns
-
-                    // Methods
-                    public function __construct($nr, $nc) {
-                        $this->nr = $nr;
-                        $this->nc = $nc;
-                    }
-
-                    public function drawTable($nr) {
-                        echo $this->startTable();
-                        echo $this->contentTable();
-                        echo $this->endTable();
-
-                    }
-
-                    private function startTable() {
-                        return '<table>';
-                    }
-
-                    private function contentTable() {
-                        return '<tr>  
-                                    <td></td>
-                                </tr>';
-                    }
-
-                    private function endTable() {
-                        return '</table>';
-                    }
-
-                }
-
-                if (isset($_POST['btn'])) {
-                    $table = new TableMaker($_POST['nr'], $_POST['nc']);
-                    $table->drawTable();
-                }
-                
-            ?>
             <h2>Table Maker</h2>
             <form action="" method="post">
-            <label>
-                <p>Rows:</p>
-                <input type="range" name="nr" min="1" max="20" step="1" value="1" oninput="o1.value=this.value">
-                <output id="o1">1</output>
-            </label>
-            <label>
-                <p>Columns:</p>
-                <input type="range" name="nc" min="1" max="20" step="1" value="1" oninput="o2.value=this.value">
-                <output id="o2">1</output>
-            </label>
-            <button name="btn"> Make Table </button>
-        </form>
+                <label>
+                    <p>Rows:</p>
+                    <input type="range" name="nr" min="1" max="20" step="1" value="1" oninput="o1.value=this.value">
+                    <output id="o1">1</output>
+                </label>
+                <label>
+                    <p>Columns:</p>
+                    <input type="range" name="nc" min="1" max="20" step="1" value="1" oninput="o2.value=this.value">
+                    <output id="o2">1</output>
+                </label>
+                <button name="btn"> Make Table </button>
+            </form>
+            <?php
+            class TableMaker
+            {
+                // Attributes
+                private $nr; //Number of Rows
+                private $nc; //Number od Columns
+
+                // Methods
+                public function __construct($nr, $nc)
+                {
+                    $this->nr = $nr;
+                    $this->nc = $nc;
+                }
+
+                public function drawTable()
+                {
+                    echo $this->startTable();
+                    echo $this->contentTable();
+                    echo $this->endTable();
+                }
+
+                private function startTable()
+                {
+                    return '<table>';
+                }
+
+                private function contentTable()
+                {
+                    $content = '';
+                    for ($i = 0; $i < $this->nr; $i++) {
+                        $content .= '<tr>';
+                        for ($j = 0; $j < $this->nc; $j++) {
+                            $content .= '<td></td>';
+                        }
+                        $content .= '</tr>';
+                    }
+                    return $content;
+                }
+
+                private function endTable()
+                {
+                    return '</table>';
+                }
+            }
+
+            if ($_POST) {
+                $nr = $_POST['nr'];
+                $nc = $_POST['nc'];
+                $table = new TableMaker($nr, $nc);
+                $table->drawTable(); 
+            }
+            ?>
         </section>
     </main>
 </body>
