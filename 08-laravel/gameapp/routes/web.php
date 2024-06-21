84d19116aaeeafe8d0285f573974cd680652c745
Juan-Posada <juanposada.0724@gmail.com>
2024-06-20 21:02:38 -0500

    Se crean una ruta y una vista

--- 08-laravel/gameapp/routes/web.php
+++ 08-laravel/gameapp/routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/game/{id}', function() {
+    $game = App\Models\Game::find(request()->id);
+    dd($game->toArray());
+});
+
+Route::get('/games/', function() {
+    $game = App\Models\Game::all();
+    return view('listgames')->with('games', $game);
+});
